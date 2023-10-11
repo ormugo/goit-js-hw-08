f64735122080e1685d1d3437817c953f303bee60
ormugo <orladismunozgomez@gmail.com>
2023-10-10 21:12:10 -0500

    GOIT Correcion Tarea 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+const galleryMarkUp = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>`
+  )
+  .join('');
+
+galleryMarkUp.insertAdjacentHTML('beforeend', galleryEl);
+
+const lightbox = new SimpleLightbox('.gallery__item a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 0,
+});
+
+galleryMarkUp.addEventListener('click', onImgClick);
+
+function onImgClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  lightbox.open();
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const formEl = document.querySelector('.feedback-form');
+const inputEl = formEl[(name = 'email')];
+const textAreaEl = formEl[(name = 'message')];
 
+savedStorageValue();
+
+formEl.addEventListener('submit', onSubmitHandler);
+formEl.addEventListener('input', throttle(onTextarealHandler, 500));
+
+function onSubmitHandler(e) {
+  e.preventDefault();
+  if (inputEl.value === '' || textAreaEl.value === '') {
+    return;
+  }
+  const savedText = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log(savedText);
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextarealHandler(e) {
+  const allData = {
+    email: inputEl.value,
+    message: textAreaEl.value,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(allData));
+  allData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(allData));
+}
+
+function savedStorageValue() {
+  const savedText = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedText) {
+    if (savedText.message) {
+      textAreaEl.value = savedText.message;
+    }
+    if (savedText.email) {
+      inputEl.value = savedText.email;
+    }
+  }
+}
